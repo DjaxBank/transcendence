88e3cc914b20a90cfcec4da4d72793da9368700c
wdpk <wdpk@wdpk-home.local>
2026-06-02 13:20:52 +0200

    Added docker-compose for the database as well as creating a separation between the DB files and the index.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,35 +1,15 @@
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { eq } from 'drizzle-orm';
-import { usersTable } from './db/schema';
-  
-const db = drizzle(process.env.DATABASE_URL!);
+import { connectToDatabase } from './db/db-test';
+import dotenv from 'dotenv';
+import path from 'path';
 
 async function main() {
-  const user: typeof usersTable.$inferInsert = {
-    name: 'John',
-    age: 30,
-    email: 'john@example.com',
-  };
+  console.log('Starting up backend');
 
-  await db.insert(usersTable).values(user);
-  console.log('New user created!')
+  // Imports .env file from the root of the repository
+  dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
-  const users = await db.select().from(usersTable);
-  console.log('Getting all users from the database: ', users)
-
-  await db
-    .update(usersTable)
-    .set({
-      age: 31,
-    })
-    .where(eq(usersTable.email, user.email));
-  console.log('User info updated!')
-  
-  console.log('Getting all users from the database: ', users)
-
-  await db.delete(usersTable).where(eq(usersTable.email, user.email));
-  console.log('User deleted!')
+  // Sets up the database, needs error handling for when the database is offline.
+  connectToDatabase();
 }
 
 main();
