6638a344534373b37e32b476cd48a3a5590ed392
elinakly <klimova.elina85@gmail.com>
2026-07-05 22:36:29 +0200

    some comments added

---
+++ my-survival-game/src/game/entities/Castle.ts
@@ -0,0 +1,1 @@
+// TODO: Implement Castle class
--- my-survival-game/vite.config.ts
+++ my-survival-game/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Vite configuration object
 export default defineConfig({
-  plugins: [react()],
+  // Vite plugins to use
+  plugins: [
+    react(),  // React plugin for JSX support and Fast Refresh
+  ],
 })
