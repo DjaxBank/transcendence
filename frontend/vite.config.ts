861cb5bc959c0a1cb1b8f11ecb09ef2167a76975
Djax Bank <djax@djax.dev>
2026-07-23 14:42:20 +0200

    change ports and hostname

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
     react(),  // React plugin for JSX support and Fast Refresh
   ],
   server: {
-    allowedHosts: ['transcendence.game'],
+    allowedHosts: ['localhost'],
   },
 })
